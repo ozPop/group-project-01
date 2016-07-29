e88418e90a57e80c9f6afd58924b1fee5698077a
ozPop <ozziepopovas@gmail.com>
2016-07-29 11:35:10 -0400

    create project structure

---
+++ config/environment.rb
@@ -0,0 +1,10 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+# classes
+require_relative "../lib/blogreader/scraper"
+require_relative "../lib/traveler/continent"
+require_relative "../lib/traveler/country"
+require_relative "../lib/traveler/post"
+require_relative "../lib/blogreader/cli"
---
+++ lib/traveler/continent.rb
@@ -0,0 +1,3 @@
+class Continent
+
+end
